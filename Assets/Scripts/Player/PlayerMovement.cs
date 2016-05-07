4c38146a0ce71bee283f941ac5f0bb7f70a39b76
Sofakinglazy <yx5m14@soton.ac.uk>
2016-05-07 14:22:43 +0100

    Added playermovement script
    - player can move up down left right
    - player can rotate with the curse moving

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public int health = 100f;
+
+	Rigidbody2D rg2d;
+	float alterDegree = -90f; //To make the tank face to x axis
+
+	void Start (){
+		rg2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update (){
+		if (health < 100) {
+			Death ();
+		}
+	}
+
+	void FixedUpdate(){
+		// Get the input from keyboard 
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+
+		// Move
+		Move(x, y);
+
+		// Get the mouse position of mouse 
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+
+		// Rotate
+		Rotate(mouseX, mouseY);
+	}
+
+	void Move(float x, float y){
+		Vector3 movement = new Vector3 (x, y, 0);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rg2d.MovePosition (transform.position + movement);
+	}
+		
+	void Rotate(float x, float y){
+		Vector3 mousePos = new Vector3 (x, y, 0);
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		Vector3 diff = mousePos - transform.position;
+		float angle = Mathf.Atan2 (diff.y, diff.x) * Mathf.Rad2Deg + alterDegree;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+
+	void Death(){
+		Debug.Log ("player is dead.");
+	}
+
+
+}
