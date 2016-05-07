97995ef61257f8fd857939823ee48ac7b91c439b
pengbill <pengbill22@gmail.com>
2016-05-07 20:32:24 +0100

    Added enemy script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Zombie;
+	Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Zombie.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Zombie.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject player;
+
+	float speed = 50f;
+	float maxDistance = 10f;
+	float range;
+	Vector2 offset;
+
+	void Start (){
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update ()
+	{
+		range = Vector2.Distance (transform.position, player.transform.position);
+
+		if (range > maxDistance) {
+			transform.position = Vector2.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+		}
+
+		Rotating ();
+	}
+
+	void Rotating ()
+	{
+		offset = player.transform.position - transform.position;
+		float angle = Mathf.Atan2 (offset.y, offset.x) * Mathf.Rad2Deg - 90f;
+		transform.rotation = Quaternion.AngleAxis (angle, new Vector3(0, 0, 1));
+	}
+}
