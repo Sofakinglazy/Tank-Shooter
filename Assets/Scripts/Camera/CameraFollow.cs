9de836f3dc8b0d6a76e9646733012664061d6404
Sofakinglazy <yx5m14@soton.ac.uk>
2016-05-07 18:05:20 +0100

    Added camera follow script - camera can follow the player and can be limmited in a boundary - found a bug that shooting ray cant stick to the mouse position all the time

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float dampTime = 0.2f;
+	public Boundary boundary;
+
+	GameObject player;
+	Vector3 velocity = Vector3.zero;
+
+	void Start (){
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update(){
+		Vector3 cameraPos = transform.position;
+		Vector3 playerPos = player.transform.position;
+
+		Vector3 relativeCenter = new Vector3 (0.5f, 0.5f, 10f);
+
+		Vector3 delta = playerPos - Camera.main.ViewportToWorldPoint (relativeCenter);
+		Vector3 destination = cameraPos + delta;
+
+		destination.x = Mathf.Clamp (destination.x, boundary.xMin, boundary.xMax);
+		destination.y = Mathf.Clamp (destination.y, boundary.yMin, boundary.yMax);
+
+		Debug.Log (destination);
+
+		transform.position = Vector3.SmoothDamp (cameraPos, destination, ref velocity, dampTime);
+	}
+}
+
+
+[System.Serializable]
+public class Boundary{
+	public float xMax;
+	public float xMin;
+	public float yMax;
+	public float yMin;
+}
