f622c486c9ae0c32cf1c6ceb79062a78d30eee40
Sofakinglazy <yx5m14@soton.ac.uk>
2016-05-09 02:48:54 +0100

    Added GameController and LevelManager scripts - Enemies can be generated by gamecontroller - Roughly list out every level's difficulty Next step: - Simulate a difficulty adjusted system - UI to show player health and score and hint for next wave

---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+	public Transform[] enemySpawn;
+	public Transform[] barrelSpawn;
+
+	public GameObject[] enemies;
+	public GameObject ammoPack;
+	public GameObject healthPack;
+	public GameObject barrel;
+
+	public Boundary boundary;
+
+	public float timeBetweenEnemySpawn;
+	float enemyTimer;
+	int enemyCount;
+
+	float pickupTimer;
+	public float timeBetweenPickupSpawn;
+	int pickupCount;
+
+	int level;
+	float[] timer;
+
+	void Start ()
+	{
+		timer = new float[3];
+		SpawnBarrels ();
+	}
+
+	void Update ()
+	{
+		enemyTimer += Time.deltaTime;
+		if (enemyTimer > timeBetweenEnemySpawn && enemyCount < LevelManager.LEVELS_PARA [4 * level]) {
+			SpawnEnemies ();
+		}
+
+		pickupTimer += Time.deltaTime;
+		if (pickupTimer > timeBetweenPickupSpawn && pickupCount < LevelManager.LEVELS_PARA [4 * level + 2]) {
+			SpawnPickup ();
+		}
+
+		timer [0] += Time.deltaTime;
+		if (timer [0] > 30f && level < 10) {
+			level++;
+			enemyCount = 0;
+			pickupCount = 0;
+			timer [0] = 0;
+			Debug.Log ("Level up");
+		}
+	}
+
+	void SpawnEnemies ()
+	{
+		int indexSpawn = GetRandomNum (0, enemySpawn.Length);
+		int indexEnemy = GetRandomNum (0, enemies.Length);
+
+		Instantiate (enemies [indexEnemy], enemySpawn [indexSpawn].position, Quaternion.identity);
+
+		enemyTimer = 0f;
+		enemyCount++;
+	}
+
+	void SpawnBarrels ()
+	{ 
+		for (int i = 0; i < barrelSpawn.Length; i++) {
+			Instantiate (barrel, barrelSpawn [i].position, Quaternion.identity);
+		}
+	}
+
+	void SpawnPickup ()
+	{
+		Vector3 randPos = new Vector3 (GetRandomNum (boundary.xMin, boundary.xMax), GetRandomNum (boundary.yMin, boundary.yMax), 0f);
+		Instantiate (ammoPack, randPos, Quaternion.identity);
+
+		pickupTimer = 0f;
+		pickupCount++;
+	}
+
+	int GetRandomNum (float min, float max)
+	{
+		float random = Random.Range (0, max);
+		return (int) Mathf.Round (random);
+	}
+}
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int[] LEVELS_PARA = { 
+					// Enemy, AmmoPack, HealthPack, ExposiveBarrel //
+		/* Level1 */	2,		1, 			1,			3,
+		/* Level2 */	4,		1,			1,			3,
+		/* Level3 */	6,		2,			2,			3,
+		/* Level4 */	8,		2,			2,			3,
+		/* Level5 */	10,		2,			2,			3,
+		/* Level6 */	12,		3,			3,			3,
+		/* Level7 */	14,		3,			3,			3,
+		/* Level8 */	16,		4,			3,			3,
+		/* Level9 */	18,		4,			3,			3,
+		/* Level0 */	20,		4,			4,			3
+	};
+}
